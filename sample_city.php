9026eb3656c53dd1c752faaf1e40190afa341bfb
Thomas J Mather <geoipapi@maxmind.com>
2007-01-10 21:09:42 +0000

    Added note that new format is needed for GeoIP City

--- sample_city.php
+++ sample_city.php
@@ -3,7 +3,10 @@
 
 // This code demonstrates how to lookup the country, region, city,
 // postal code, latitude, and longitude by IP Address.
-// It is designed to work with GeoIP City Edition available from MaxMind
+// It is designed to work with GeoIP/GeoLite City
+
+// Note that you must download the New Format of GeoIP City (GEO-133).
+// The old format (GEO-132) will not work.
 
 include("geoipcity.inc");
 
