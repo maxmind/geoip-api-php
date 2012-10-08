f3dceb2d87f651c90ae045661745b922d244f299
Gregory Oschwald <goschwald@maxmind.com>
2012-10-08 14:28:11 -0700

    Added a domain sample

---
+++ sample_domain.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php -q
+<?php
+
+// This code demonstrates how to lookup the country and region by IP Address
+// It is designed to work with GeoIP Organization or GeoIP ISP available from MaxMind
+
+include("geoip.inc");
+
+$gi = geoip_open("/usr/local/share/GeoIP/GeoIPDomain.dat",GEOIP_STANDARD);
+
+$domain = geoip_org_by_addr($gi,"80.24.24.24");
+print "80.24.24.24 belongs to " . $domain . "\n";
+
+geoip_close($gi);
+
+?>
+
