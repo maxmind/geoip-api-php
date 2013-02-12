a76108cce8cb5dc230635e7963bb857d3813107c
Boris Zentner <bzentner@maxmind.com>
2013-02-12 13:52:12 +0100

    Remove weird trailing spaces

--- timezone/make_time_zone_php_code.pl
+++ timezone/make_time_zone_php_code.pl
@@ -8,9 +8,9 @@ my $old_region;
 # Obtain timezone.txt from http://www.maxmind.com/timezone.txt
 open(FILE,"timezone.txt");
 my $str = <FILE>;
-print "<?php \n";
+print "<?php\n";
 print "function get_time_zone(\$country,\$region) {\n";
-print "  switch (\$country) { \n";
+print "  switch (\$country) {\n";
 
 while ($str = <FILE>) {
   $str =~ s!\s*$!!; 
@@ -18,12 +18,12 @@ while ($str = <FILE>) {
   #print $country . "," . $region . "," . $timezone . "\n";
   if ($country ne $old_country) {
     if ($old_region ne "") {
-      print "  } \n";
+      print "  }\n";
       print "  break;\n";
     }
     print "case " . qq(") . $country . qq(") . ":\n";
     if ($region ne "") {
-      print "    switch (\$region) { \n";
+      print "    switch (\$region) {\n";
     }
   }
   if ($region ne "") {
@@ -38,11 +38,11 @@ while ($str = <FILE>) {
   $old_country = $country;
   $old_region = $region;
 }
-print "  } \n";
-print "  return \$timezone; \n";
+print "  }\n";
+print "  return \$timezone;\n";
 
-print "} \n";
+print "}\n";
 
-print "?> \n";
+print "?>\n";
 
 close(FILE);
